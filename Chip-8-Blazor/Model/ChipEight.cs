476e69b8e1e0ad2d81355793cf49dab1d7a817f6
Thomas <perzit@edu.htl-villach.at>
2022-08-18 15:08:17 +0200

    Added some opcodes

--- Chip-8-Blazor/Model/ChipEight.cs
+++ Chip-8-Blazor/Model/ChipEight.cs
@@ -2,39 +2,163 @@
 {
     public class ChipEight
     {
-        public ushort opcode { get; private set; } // Store opcode, 2 Bytes
+        public ushort CurrentOpcode { get; private set; } // Store opcode, 2 Bytes
         private byte[] memory = new byte[4096];
         private byte[] v = new byte[16];
+
+        private Dictionary<byte, Action<OpCode>> opCodes;
+        private Dictionary<byte, Action<OpCode>> logicOpCodes;
 
         private ushort index;
         private ushort pc;
 
-        private byte[] gfx = new byte[64 * 32]; // Screen pixels
+        public byte[] Gfx { get; } = new byte[64 * 32]; // Screen pixels
 
         private byte delayTimer;
         private byte soundTimer;
 
-        private byte[] stack = new byte[16];
+        private ushort[] stack = new ushort[16];
         private byte stackPointer;
 
         private byte[] keyboard = new byte[16];
 
         public ChipEight()
         {
-            for(int i = 0; i < memory.Length; i++)
+            pc = 0x200;
+
+            opCodes = new Dictionary<byte, Action<OpCode>>()
             {
-                memory[i] = 0xFF;
-            }
+                { 0x0, LeadingZeroCodes },
+                { 0x1, JumpToAdress },
+                { 0x2, JumpToSubroutine },
+                { 0x3, SkipIfRegisterEqual },
+                { 0x4, SkipIfRegisterNotEqual },
+                { 0x5, SkipIfTwoRegistersEqual },
+                { 0x6, PutValueInRegister },
+                { 0x7, AddToRegister }
+            };
 
-            memory[0x0011] = 0xAA;
-            memory[0x0011 + 1] = 0x12;
+            logicOpCodes = new Dictionary<byte, Action<OpCode>>()
+            {
 
-            pc = 0x0011;
+            };
         }
 
         public void Cycle()
         {
-            opcode = (ushort)(memory[pc] << 8 | memory[pc + 1]);
+            CurrentOpcode = (ushort)(memory[pc] << 8 | memory[pc + 1]);
         }
+
+        private void HandleOpCode()
+        {
+
+        }
+
+        private void LeadingZeroCodes(OpCode opCode)
+        {
+            if (opCode.Opcode == 0x00E0)
+            {
+                ClearScreen();
+                return;
+            }
+
+            if (opCode.Opcode == 0x00EE)
+            {
+                // todo: Return from subroutine
+                return;
+            }
+        }
+        private void ClearScreen()
+        {
+            for (int i = 0; i < Gfx.Length; i++)
+            {
+                Gfx[i] = 0x0000;
+            }
+        }
+
+        private void JumpToAdress(OpCode opCode)
+        {
+            ushort adress = opCode.NNN;
+            pc = adress;
+        }
+
+        private void JumpToSubroutine(OpCode opCode)
+        {
+            stack[stackPointer] = pc;
+            stackPointer++;
+
+            pc = opCode.NNN;
+        }
+
+        private void SkipIfRegisterEqual(OpCode opCode)
+        {
+            int regIndex = opCode.X;
+
+            if (v[regIndex] == opCode.KK)
+            {
+                pc = (ushort)(pc + 2);
+            }
+        }
+
+        private void SkipIfRegisterNotEqual(OpCode opCode)
+        {
+            int regIndex = opCode.X;
+
+            if (v[regIndex] != opCode.KK)
+            {
+                pc = (ushort)(pc + 2);
+            }
+        }
+
+        private void SkipIfTwoRegistersEqual(OpCode opCode)
+        {
+            if(opCode.N == 0)
+            {
+                int regIndexOne = opCode.X;
+                int regIndexTwo = opCode.Y;
+
+                if (v[regIndexOne] == v[regIndexTwo])
+                {
+                    pc = (ushort)(pc + 2);
+                }
+            }
+        }
+
+        private void PutValueInRegister(OpCode opCode)
+        {
+            int regIndex = opCode.X;
+            v[regIndex] = opCode.KK;
+        }
+
+        private void AddToRegister(OpCode opCode)
+        {
+            int regIndex = opCode.X;
+            v[regIndex] += opCode.KK;
+        }
+
+        private void SetRegisterXToY(OpCode opCode)
+        {
+            int regIndexOne = opCode.X;
+            int regIndexTwo = opCode.Y;
+
+            v[regIndexOne] = v[regIndexTwo];
+        }
+
+        private void BitwiseOr(OpCode opCode)
+        {
+            int regIndexOne = opCode.X;
+            int regIndexTwo = opCode.Y;
+
+            v[regIndexOne] = (byte)(v[regIndexOne] | v[regIndexTwo]);
+        }
+
+        private void BitwiseAnd(OpCode opCode)
+        {
+            int regIndexOne = opCode.X;
+            int regIndexTwo = opCode.Y;
+
+            v[regIndexOne] = (byte)(v[regIndexOne] & v[regIndexTwo]);
+        }
+
     }
 }
---
+++ Chip-8-Blazor/Model/OpCode.cs
@@ -0,0 +1,25 @@
+﻿namespace Chip_8_Blazor.Model
+{
+    public class OpCode
+    {
+        public ushort Opcode { get; }
+
+        public ushort NNN { get; }
+        public byte X { get; }
+        public byte Y { get; }
+        public byte KK { get; }
+        public ushort N { get; }
+
+        public OpCode(ushort opcode)
+        {
+            Opcode = opcode;
+
+            NNN = (ushort)(opcode & 0x0FFF);
+            X = (byte)((opcode & 0x0F00) >> 8);
+            Y = (byte)((opcode & 0x00F0) >> 4);
+            KK = (byte)(opcode & 0x00FF);
+            N = (byte)(opcode & 0x000F);
+
+        }
+    }
+}
